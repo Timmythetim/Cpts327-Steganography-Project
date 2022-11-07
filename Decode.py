ee1c868bd6494decfad4b342d5958fb55e889ffd
Timmythetim <tim.reidy@wsu.edu>
2022-11-06 17:35:36 -0800

    Basic encoding/decoding added, I should make it a little more fluid though

---
+++ Decode.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import tifffile as tf
+
+x = 0
+y = 0
+z = 0
+m = 0
+
+array = tf.imread("keysencoded.tiff")
+
+delimeter = "1111111111111111"
+
+message = ""
+counter = 0
+print(array[0])
+while delimeter != "0000000000000000":
+    binary = format((array[x][y][z]), '08b')
+    last = binary[-1]
+    # print(last)
+    # input("Press Enter to continue...")
+    z += 1
+    if z == 3:
+        y += 1
+        z = 0
+    if y == len(array[0]):
+        x += 1
+        y = 0
+    delimeter = delimeter[1:]
+    delimeter += last
+    message += last
+    counter += 1
+    if counter == 8:
+        message += " "
+        counter = 0
+print(message)
---
+++ Encode.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import tifffile as tf
+import numpy
+
+# image = img.imread("keys.png")
+
+array = tf.imread("keys.tiff")
+file = open("Message.txt", "r")
+message = file.read()
+message += "0000000000000000"
+x = 0
+y = 0
+z = 0
+m = 0
+print(type(array[x][y][z]))
+for x in range(0,10):
+    print(array[0][x])
+print("\n")
+x = 0
+while m < len(message):
+    if message[m] != " ":
+        binary = format((array[x][y][z]), '08b')   
+        last = binary[-1]
+        if last != message[m]:
+            binary = binary[:-1]
+            binary += message[m]
+            number = "0b" + binary
+            number = int(binary,2)
+            array[x][y][z] = number
+        z += 1
+        if z == 3:
+            y += 1
+            z = 0
+        if y == len(array[0]):
+            x += 1
+            y = 0
+            z = 0
+    m += 1
+tf.imwrite("keysencoded.tiff",array)
