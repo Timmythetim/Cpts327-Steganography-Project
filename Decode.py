be20227cf172a72f2f4113a2e1a80b904eca63db
Timmythetim <tim.reidy@wsu.edu>
2022-11-07 23:47:16 -0800

    Works perfect for plain text, now I would like to be able to embed any file

--- Decode.py
+++ Decode.py
@@ -6,31 +6,47 @@ x = 0
 y = 0
 z = 0
 m = 0
+character = ""
+finalmessage = ""
 
-array = tf.imread("testencoded.tiff")
+string = input("Please enter local file path of encoded image\n")
 
-delimeter = "1111111111111111"
+try:
+    array = tf.imread(string)
 
-message = ""
-counter = 0
-print(array[0])
-while delimeter != "0000000000000000":
-    binary = format((array[x][y][z]), '08b')
-    last = binary[-1]
-    # print(last)
-    # input("Press Enter to continue...")
-    z += 1
-    if z == 3:
-        y += 1
-        z = 0
-    if y == len(array[0]):
-        x += 1
-        y = 0
-    delimeter = delimeter[1:]
-    delimeter += last
-    message += last
-    counter += 1
-    if counter == 8:
-        message += " "
-        counter = 0
-print(message)
+    delimeter = "1111111111111111"
+
+    message = ""
+    counter = 0
+    print(array[0])
+    while delimeter != "0000000000000000":
+        binary = format((array[x][y][z]), '08b')
+        last = binary[-1]
+        # print(last)
+        # input("Press Enter to continue...")
+        z += 1
+        if z == 3:
+            y += 1
+            z = 0
+        if y == len(array[0]):
+            x += 1
+            y = 0
+        delimeter = delimeter[1:]
+        delimeter += last
+        message += last
+        # counter += 1
+        # if counter == 8:
+        #     message += " "
+        #     counter = 0
+except:
+    print("File not found!")
+x = 0
+while x+8 < len(message):
+    character = message[x:x+8]
+    character = int(character,2)
+    character = chr(character)
+    finalmessage += character
+    x += 8
+
+
+print(finalmessage)
--- Encode.py
+++ Encode.py
@@ -1,40 +1,89 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 import tifffile as tf
-import numpy
-
+import magic
 # image = img.imread("keys.png")
+stringpic = input("Please enter local file path of image to encode\n")
+string = input("Please enter local file path of file to embed, any file type is acceptable\n")
+if "text" in magic.from_file(string,mime=True):
+    file = open(string, "r")
 
-array = tf.imread("test.tiff")
-file = open("Message.txt", "r")
-message = file.read()
-message += "0000000000000000"
-x = 0
-y = 0
-z = 0
-m = 0
-print(type(array[x][y][z]))
-for x in range(0,10):
-    print(array[0][x])
-print("\n")
-x = 0
-while m < len(message):
-    if message[m] != " ":
-        binary = format((array[x][y][z]), '08b')   
-        last = binary[-1]
-        if last != message[m]:
-            binary = binary[:-1]
-            binary += message[m]
-            number = "0b" + binary
-            number = int(binary,2)
-            array[x][y][z] = number
-        z += 1
-        if z == 3:
-            y += 1
-            z = 0
-        if y == len(array[0]):
-            x += 1
-            y = 0
-            z = 0
-    m += 1
-tf.imwrite("testencoded.tiff",array)
+    array = tf.imread(stringpic)
+
+    plaintext = file.read()
+    message = ""
+    for x in plaintext:
+        message += format((ord(x)), '08b')
+    message += "0000000000000000"
+    print(message)
+    x = 0
+    y = 0
+    z = 0
+    m = 0
+    print(type(array[x][y][z]))
+    for x in range(0,10):
+        print(array[0][x])
+    print("\n")
+    x = 0
+    while m < len(message):
+        if message[m] != " ":
+            binary = format((array[x][y][z]), '08b')   
+            last = binary[-1]
+            if last != message[m]:
+                binary = binary[:-1]
+                binary += message[m]
+                number = "0b" + binary
+                number = int(binary,2)
+                array[x][y][z] = number
+            z += 1
+            if z == 3:
+                y += 1
+                z = 0
+            if y == len(array[0]):
+                x += 1
+                y = 0
+                z = 0
+        m += 1
+    tf.imwrite("testencoded.tiff",array)
+else:
+    file = open(string, "br")
+
+    array = tf.imread(stringpic)
+
+    plaintext = file.read()
+    message = ""
+    print(str(plaintext[2:32]))
+    for x in plaintext:
+        message += str(x)
+        input(x)
+    message += "0000000000000000"
+    print(message)
+    x = 0
+    y = 0
+    z = 0
+    m = 0
+    print(type(array[x][y][z]))
+    for x in range(0,10):
+        print(array[0][x])
+    print("\n")
+    x = 0
+    while m < len(message):
+        if message[m] != " ":
+            binary = format((array[x][y][z]), '08b')   
+            last = binary[-1]
+            if last != message[m]:
+                binary = binary[:-1]
+                binary += message[m]
+                number = "0b" + binary
+                number = int(binary,2)
+                array[x][y][z] = number
+            z += 1
+            if z == 3:
+                y += 1
+                z = 0
+            if y == len(array[0]):
+                x += 1
+                y = 0
+                z = 0
+        m += 1
+    tf.imwrite("testencoded.tiff",array)
