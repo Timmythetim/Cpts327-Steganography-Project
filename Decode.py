59683325c8b498e2fb44d81420cc41bd4413365d
Timmythetim <tim.reidy@wsu.edu>
2022-11-11 18:03:27 -0800

    This is a WIP build, text file encoding works, and the message survives email over outlook, but I would love to be able to encode other types of files

--- Decode.py
+++ Decode.py
@@ -6,20 +6,17 @@ x = 0
 y = 0
 z = 0
 m = 0
-character = ""
+message = ""
 finalmessage = ""
+counter = 0
 
 string = input("Please enter local file path of encoded image\n")
-
+size = ""
 try:
     array = tf.imread(string)
 
-    delimeter = "1111111111111111"
 
-    message = ""
-    counter = 0
-    print(array[0])
-    while delimeter != "0000000000000000":
+    while counter < 32:
         binary = format((array[x][y][z]), '08b')
         last = binary[-1]
         # print(last)
@@ -31,15 +28,29 @@ try:
         if y == len(array[0]):
             x += 1
             y = 0
-        delimeter = delimeter[1:]
-        delimeter += last
+        size += last
+        counter += 1
+    counter = 0
+    print(int(size,2))
+    size = int(size,2)
+    while counter < size*8 + 8:
+        binary = format((array[x][y][z]), '08b')
+        last = binary[-1]
+        # print(last)
+        # input("Press Enter to continue...")
+        z += 1
+        if z == 3:
+            y += 1
+            z = 0
+        if y == len(array[0]):
+            x += 1
+            y = 0
         message += last
-        # counter += 1
-        # if counter == 8:
-        #     message += " "
-        #     counter = 0
+        counter += 1
+
 except:
     print("File not found!")
+
 x = 0
 while x+8 < len(message):
     character = message[x:x+8]
@@ -48,5 +59,6 @@ while x+8 < len(message):
     finalmessage += character
     x += 8
 
-
+file = open("output", "w")
+file.write(finalmessage)
 print(finalmessage)
--- Encode.py
+++ Encode.py
@@ -2,19 +2,23 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 import tifffile as tf
 import magic
+from bitstring import BitArray
 # image = img.imread("keys.png")
 stringpic = input("Please enter local file path of image to encode\n")
 string = input("Please enter local file path of file to embed, any file type is acceptable\n")
+length = 0
+# try:
 if "text" in magic.from_file(string,mime=True):
     file = open(string, "r")
 
     array = tf.imread(stringpic)
 
     plaintext = file.read()
-    message = ""
+    length = len(plaintext)
+    message = '{:032b}'.format(length)
+    print(message)
     for x in plaintext:
         message += format((ord(x)), '08b')
-    message += "0000000000000000"
     print(message)
     x = 0
     y = 0
@@ -51,39 +55,42 @@ else:
     array = tf.imread(stringpic)
 
     plaintext = file.read()
-    message = ""
-    print(str(plaintext[2:32]))
-    for x in plaintext:
-        message += str(x)
-        input(x)
-    message += "0000000000000000"
-    print(message)
+    length = len(plaintext)
+    print(plaintext[:64])
+
+    message = '{:032b}'.format(length)
+    for x in range(0,len(plaintext)):
+        temp = '{:08b}'.format(plaintext[x])
+        message += temp
     x = 0
     y = 0
     z = 0
     m = 0
-    print(type(array[x][y][z]))
-    for x in range(0,10):
-        print(array[0][x])
-    print("\n")
     x = 0
     while m < len(message):
-        if message[m] != " ":
-            binary = format((array[x][y][z]), '08b')   
-            last = binary[-1]
-            if last != message[m]:
-                binary = binary[:-1]
-                binary += message[m]
-                number = "0b" + binary
-                number = int(binary,2)
-                array[x][y][z] = number
-            z += 1
-            if z == 3:
-                y += 1
-                z = 0
-            if y == len(array[0]):
-                x += 1
-                y = 0
-                z = 0
+        if x == len(array):
+            print("Encoded File is too large to put into this image! Choose a bigger image or smaller file!")
+            break
+        binary = format((array[x][y][z]), '08b')   
+        last = binary[-1]
+        if last != message[m]:
+            binary = binary[:-1]
+            binary += message[m]
+            number = "0b" + binary
+            number = int(binary,2)
+            array[x][y][z] = number
+        z += 1
+        if z == 3:
+            y += 1
+            z = 0
+        if y == len(array[0]):
+            x += 1
+            y = 0
+            z = 0
         m += 1
-    tf.imwrite("testencoded.tiff",array)
+    if x != len(array):
+        tf.imwrite("testencoded.tiff",array)
+        print(message[:32])
+        print(message)
+# # except:
+# #     print("An error has occurred, check your file names!")
